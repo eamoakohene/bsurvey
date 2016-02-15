83cdee57628dd86ca8a0ce68442d616f6675c14d
Emmanuel Amoakohene <eamoakohene@gmail.com>
2016-02-15 01:59:08 +0000

    Code update

--- inst/survey/surveyapp/ui_tabs_company_information.R
+++ inst/survey/surveyapp/ui_tabs_company_information.R
@@ -4,7 +4,7 @@ get_ui_tab_company_labour <- function(){
   get_ui_tab_template(
     tab="company_labour",qn=19,
     yr_range= slider_range_yr,yr_select=slider_select_yr,
-    qtr_range=slider_range_qtr, qtr_select=c(3,4),
+    qtr_range=slider_range_qtr, qtr_select= slider_select_qtr,
     is_pc = TRUE, trend_name = "company labour"
   )
 
@@ -16,7 +16,7 @@ get_ui_tab_company_sector <- function(){
   get_ui_tab_template(
     tab="company_sector",qn=20,
     yr_range= slider_range_yr,yr_select=slider_select_yr,
-    qtr_range=slider_range_qtr, qtr_select=c(3,4),
+    qtr_range=slider_range_qtr, qtr_select= slider_select_qtr,
     is_pc = TRUE, trend_name = "company sector"
   )
 
@@ -28,7 +28,7 @@ get_ui_tab_company_turnover <- function(){
   get_ui_tab_template(
     tab="company_turnover",qn=21,
     yr_range= slider_range_yr,yr_select=slider_select_yr,
-    qtr_range=slider_range_qtr, qtr_select=c(3,4),
+    qtr_range=slider_range_qtr, qtr_select= slider_select_qtr,
     is_pc = TRUE, trend_name = "company_turnover"
   )
 
